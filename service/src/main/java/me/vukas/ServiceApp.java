491a4c4b399045ed17a1fda0544e1307d60e0c3e
Bojan Vukasovic <b.vukasovic@sportradar.com>
2018-04-27 22:32:13 +0200

    added refreshing of properties using @RefreshScope

--- service/src/main/java/me/vukas/ServiceApp.java
+++ service/src/main/java/me/vukas/ServiceApp.java
@@ -2,10 +2,8 @@ package me.vukas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories("me.vukas")
 public class ServiceApp {
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceApp.class, args);
--- service/src/main/java/me/vukas/ServiceConfig.java
+++ service/src/main/java/me/vukas/ServiceConfig.java
@@ -1,9 +1,11 @@
 package me.vukas;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @Component
+@RefreshScope	//accessing this via POST to /actuator/refresh endpoint. Refreshes someProp if there is new value on config server.
 public class ServiceConfig {
 	@Value("${some.prop}")
 	private String someProp;
